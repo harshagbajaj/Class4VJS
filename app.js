6ce7a0cfe6c3070948c519777a5abdd851fab1d6
harshagbajaj <harshagbajaj@gmail.com>
2020-12-09 10:52:06 +0530

    input processing output done.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-console.log("hello");
-prompt("enter name");
-alert("this scrit works");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslated(inputText){
+     return serverURL + "?" + "text="+ inputText;
+}
+
+function errorHandler(error){
+     console.log("error occured", error);     
+}
+
+function clickHandler() {
+     var inputText = txtInput.value;
+
+     fetc
+};	
+
+btnTranslate.addEventListener("click", clickHandler)
